aba44f060580d4148ba335da4827d8c320eb49a8
kisara579 <kisarabeddawala@gmail.com>
2025-11-14 01:55:59 +0530

    Implement client socket connection in main.py

--- client/main.py
+++ client/main.py
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import socket
+
+server_ip = "20.205.16.74"
+server_port = 8888
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+try:
+    client_socket.connect((server_ip,server_port))
+    print("Connected to server")
+except Exception as e:
+    print("Connection Failed:",e)
+    exit()
+
+    
